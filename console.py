f37fa117b8196f89f67ee093ae6501d89974815e
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2024-11-20 14:23:52 +0100

    Optimized Code; Output Fixes

--- console.py
+++ console.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import json, re
 
 task_operations = ['add', 'update', 'list', 'delete',
-                   'mark-in-progress', 'mark-done']
+                   'mark-in-progress', 'mark-done', 'exit']
 
 
 
@@ -19,6 +19,10 @@ while True:
     print(stored_tasks[-1])
 
     first_display = True # Using this to display useful first-launch info
+    if first_display == True:
+        print("Welcome to Task Man CLI")
+        first_display = False
+
     await_raw_input = "(task_man)$ "
     raw_input = input(await_raw_input)
 
@@ -30,10 +34,9 @@ while True:
     else:
         continue
 
-    first_display = False
-
     if user_input[0] not in task_operations:
         print(f"Unknown command \'{user_input[0]}\'")
+        continue
 
     if user_input[0] == "exit":
         print("Quitting console...")
@@ -43,7 +46,6 @@ while True:
         if len(user_input) < 2 : # This is not airtight
             print("Syntax: add \"task_description\"")
             continue
-        # elif 
 
         task = {
             "id": "",
@@ -72,23 +74,12 @@ while True:
         task_id = int(user_input[1]) - 1
 
         stored_tasks[task_id]["description"] = user_input[2]
-        # task = {
-        #     "id": "",
-        #     "description": "",
-        #     "status": "",
-        #     "createdAt": "",
-        #     "updatedAt": "",
-        # }
 
-        # task["id"] = str(len(stored_tasks) + 1)
-        # task["description"] = user_input[1]
         stored_tasks[task_id]["updatedAt"] = str(datetime.now().strftime("%x, %H:%M"))
-
-        # stored_tasks.append(task)
 
         with open("storage.json", "w") as storage:
             json.dump(stored_tasks, storage, indent=2)
-            print(f"Task updated successfully (ID: {task_id})")
+            print(f"Task updated successfully (ID: {task_id + 1})")
 
     if user_input[0] == "delete" :
         print(user_input)
@@ -103,17 +94,15 @@ while True:
             print("Syntax: delete \"task_id\"")
             continue
 
-        # task_id = int(user_input[1]) - 1
-
-        try:
-            deleted_task = stored_tasks.pop(task_id)["description"]
-        except:
-            print("Delete Unsuccessful")
+        if task_id not in range(len(stored_tasks)):
+            print("Task ID out of range")
             continue
+
+        deleted_task = stored_tasks.pop(task_id)["description"]
 
         with open("storage.json", "w") as storage:
             json.dump(stored_tasks, storage, indent=2)
-            print(f"Task deleted successfully (ID: {task_id}) \"{deleted_task}\"")
+            print(f"Task deleted successfully (ID: {task_id + 1}) - \"{deleted_task}\"")
 
     
 
